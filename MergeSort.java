6bcd8bfe2f69192a7edf646cd3ae01d263240150
karthiga0456 <727823tucs340@skct.edu.in>
2025-07-31 14:44:11 +0530

    Added sorting algorithms and main app

---
+++ MergeSort.java
@@ -0,0 +1,36 @@
+public class MergeSort {
+    public static void sort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int[] leftArr = new int[mid - left + 1];
+        int[] rightArr = new int[right - mid];
+
+        for (int i = 0; i < leftArr.length; i++)
+            leftArr[i] = arr[left + i];
+        for (int j = 0; j < rightArr.length; j++)
+            rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < leftArr.length && j < rightArr.length) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < leftArr.length)
+            arr[k++] = leftArr[i++];
+        while (j < rightArr.length)
+            arr[k++] = rightArr[j++];
+    }
+}
