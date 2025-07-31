c435243e6decf29a9882ada24adc26aca7261d1e
karthiga0456 <149648642+karthiga0456@users.noreply.github.com>
2025-07-31 14:15:37 +0530

    Update Soring.java

--- Soring.java
+++ Soring.java
@@ -1,63 +1,49 @@
 import java.util.Scanner;
 
 public class SortingApp {
-
-    // Bubble Sort
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-    }
-
-    // Insertion Sort
-    public static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int key = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j+1] = arr[j];
-                j = j - 1;
-            }
-            arr[j+1] = key;
-        }
-    }
-
-    // Print Array
-    public static void printArray(int[] arr) {
-        for (int num : arr)
-            System.out.print(num + " ");
-        System.out.println();
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        // Get array input from user
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
 
-        System.out.println("Choose Sorting Algorithm:");
+        System.out.print("Enter " + n + " integers: ");
+        for (int i = 0; i < n; i++)
+            arr[i] = scanner.nextInt();
+
+        // Choose sorting algorithm
+        System.out.println("\nChoose Sorting Algorithm:");
         System.out.println("1. Bubble Sort");
         System.out.println("2. Insertion Sort");
-        System.out.print("Enter your choice (1-2): ");
+        System.out.println("3. Merge Sort");
+        System.out.println("4. Quick Sort");
+        System.out.print("Enter your choice (1-4): ");
         int choice = scanner.nextInt();
 
+        // Sort based on choice
         switch (choice) {
             case 1:
-                bubbleSort(arr);
+                BubbleSort.sort(arr);
                 break;
             case 2:
-                insertionSort(arr);
+                InsertionSort.sort(arr);
+                break;
+            case 3:
+                MergeSort.sort(arr, 0, arr.length - 1);
+                break;
+            case 4:
+                QuickSort.sort(arr, 0, arr.length - 1);
                 break;
             default:
                 System.out.println("Invalid choice!");
                 return;
         }
 
-        System.out.println("Sorted Array:");
-        printArray(arr);
+        // Display result
+        System.out.println("\nSorted array:");
+        for (int num : arr)
+            System.out.print(num + " ");
     }
 }
